1b6250fee6395311d15307574232ba51e29d9727
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-11-25 05:28:25 -0800

    1st attempt

---
+++ Program1_number_sorter.py
@@ -0,0 +1,16 @@
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+print("VIEN ANGELO BERNALES | BSCOE 1-1 \n")
+# ask user input
+uno = int(input("ENTER FIRST NUMBER: "))
+dos = int(input("ENTER SECOND NUMBER: "))
+tres = int(input("ENTER THIRD NUMBER: "))
+quatro = int(input("ENTER FOURTH NUMBER: "))
+
+if uno > dos:
+    highest = uno
+else:
+    highest = dos
+
